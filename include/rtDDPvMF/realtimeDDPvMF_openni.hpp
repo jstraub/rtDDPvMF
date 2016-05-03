75b975dd9bd9c5a0651822da2493d188c668cfaa
Julian Straub <jstraub@csail.mit.edu>
2016-05-03 14:16:12 -0400

    demo changes

--- include/rtDDPvMF/realtimeDDPvMF_openni.hpp
+++ include/rtDDPvMF/realtimeDDPvMF_openni.hpp
@@ -18,7 +18,7 @@ class RealtimeDDPvMF : public cudaPcl::OpenniVisualizer
         w, uint32_t h);
 
   protected:
-    virtual void visualizeNormals();
+    virtual void visualizePC();
 
    shared_ptr<RtDDPvMF> pRtDdpvMF_;
 
@@ -36,15 +36,21 @@ RealtimeDDPvMF::~RealtimeDDPvMF()
 void RealtimeDDPvMF::depth_cb(const uint16_t* depth, uint32_t w,
     uint32_t h)
 {
+  static uint32_t i=0;
   Matrix3f dR = pRtDdpvMF_->applyConstVelModel();
   pRtDdpvMF_->compute(depth,w,h);
+  if (i++ > 10) {
+    cout<<"visualize Normals"<<endl;
+    cv::Mat Iseg = pRtDdpvMF_->overlaySeg(this->rgb_);
+    cv::imshow("seg",Iseg);
+  }
   this->update();
 }
 
-void RealtimeDDPvMF::visualizeNormals()
+void RealtimeDDPvMF::visualizePC()
 {
-  cout<<"visualize Normals"<<endl;
-  cv::Mat Iseg = pRtDdpvMF_->overlaySeg(this->rgb_);
-  cv::imshow("seg",Iseg);
+//  cout<<"visualize Normals"<<endl;
+//  cv::Mat Iseg = pRtDdpvMF_->overlaySeg(this->rgb_);
+//  cv::imshow("seg",Iseg);
 }
 
